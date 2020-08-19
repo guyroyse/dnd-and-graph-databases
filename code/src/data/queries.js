e798750f5d3bae7137ddaf511f909822b9e78599
Guy Royse <guy@guyroyse.com>
2020-08-18 22:51:57 -0400

    now, with data

---
+++ code/src/data/queries.js
@@ -0,0 +1,84 @@
+const graph = require('./redis-executor')
+
+async function saveDungeon(dungeon) {
+  let query = `
+    CREATE (d:dungeon)
+    SET d.name = $name
+    RETURN id(d)`
+  let values = await graph.executeAndReturnSingle(query, { name: dungeon.name })
+  dungeon.id = values[0]
+}
+
+async function saveRoom(room) {
+  let query = `
+    CREATE (r:room)
+    SET r.name = $name
+    RETURN id(r)`
+  let values = await graph.executeAndReturnSingle(query, { name: room.name })
+  room.id = values[0]
+}
+
+async function saveMonster(monster) {
+  let query = `
+    CREATE (m:monster)
+    SET m.name = $name, m.cr = $cr, m.xp = $xp
+    RETURN id(m)`
+  let values = await graph.executeAndReturnSingle(query, { 
+    name: monster.name, cr: monster.cr, xp: monster.xp })
+  monster.id = values[0]
+}
+
+async function saveTreasureItem(treasure) {
+  let query = `
+    CREATE (t:treasure)
+    SET t.name = $name, t.gp = $gp
+    RETURN id(t)`
+  let values = await graph.executeAndReturnSingle(query, { 
+    name: treasure.name, gp: treasure.gp })
+  treasure.id = values[0]
+}
+
+async function addEntrance(dungeon, room) {
+  let query = `
+    MATCH (d:dungeon) WHERE id(d) = $dungeonId
+    MATCH (r:room) WHERE id(r) = $roomId
+    CREATE (d)-[:has_entrance]->(r)`
+  await graph.execute(query, { dungeonId: dungeon.id, roomId: room.id })
+}
+
+async function addExit(dungeon, room) {
+  let query = `
+    MATCH (d:dungeon) WHERE id(d) = $dungeonId
+    MATCH (r:room) WHERE id(r) = $roomId
+    CREATE (r)-[:has_exit]->(d)`
+  await graph.execute(query, { dungeonId: dungeon.id, roomId: room.id })
+}
+
+async function addMonsterToRoom(room, monster) {
+  let query = `
+    MATCH (r:room) WHERE id(r) = $roomId
+    MATCH (m:monster) WHERE id(m) = $monsterId
+    CREATE (r)-[:contains]->(m)`
+  await graph.execute(query, { roomId: room.id, monsterId: monster.id })
+}
+
+async function addTreasureToRoom(room, treasure) {
+  let query = `
+    MATCH (r:room) WHERE id(r) = $roomId
+    MATCH (t:treasure) WHERE id(t) = $treasureId
+    CREATE (r)-[:contains]->(t)`
+  await graph.execute(query, { roomId: room.id, treasureId: treasure.id })
+}
+
+async function connectRooms(from, to) {
+  let query = `
+    MATCH (from:room) WHERE id(from) = $fromId
+    MATCH (to:room) WHERE id(to) = $toId
+    CREATE (from)-[:leads_to]->(to)`
+  await graph.execute(query, { fromId: from.id, toId: to.id })
+}
+
+
+module.exports = {
+  saveDungeon, saveRoom, saveMonster, saveTreasureItem,
+  addEntrance, addExit, connectRooms, addMonsterToRoom, addTreasureToRoom }
---
+++ code/src/data/redis-executor.js
@@ -0,0 +1,48 @@
+const RedisGraph = require('redisgraph.js').Graph
+
+const HOST = process.env.REDIS_GRAPH_HOST || 'localhost'
+const PORT = process.env.REDIS_GRAPH_PORT || 6379
+const PASSWORD = process.env.REDIS_GRAPH_PASSWORD || 'foobared'
+
+let graphClient
+
+function open(key) {
+  if (!graphClient) {
+    graphClient = new RedisGraph(key, HOST, PORT, { password: PASSWORD })
+    graphClient.deleteGraph()
+  }
+}
+
+function close() {
+  graphClient.close()
+}
+
+async function execute(query, parameters) {
+  await graphClient.query(query, parameters)
+}
+
+async function executeAndReturnSingle(query, parameters) {
+  let result = await graphClient.query(query, parameters)
+  if (result.hasNext() === false) return null
+  
+  let record = result.next()
+  if (record.size() <= 0) return null
+
+  return record.values()
+}
+
+async function executeAndReturnMany(query, parameters) {
+  let result = await graphClient.query(query, parameters)
+
+  let valueSet = []
+  while (result.hasNext()) {
+    let record = result.next()
+    if (record.size() > 0) {
+      valueSet.push(record.values())
+    }
+  }
+
+  return valueSet
+}
+
+module.exports = { open, close, execute, executeAndReturnSingle, executeAndReturnMany }
