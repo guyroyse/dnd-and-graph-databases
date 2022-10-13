e86ae62fa83e0c93c186a430930edee88212afd8
Guy Royse <guy@guyroyse.com>
2022-10-13 11:55:34 +0200

    minor upgrades to the demo

--- code/demo.js
+++ code/demo.js
@@ -4,13 +4,24 @@ const client = createClient()
 client.on('error', (err) => console.log('Redis Client Error', err))
 await client.connect()
 
+let result, query
 
-let result = await client.graph.query('dungeon', `
-  MATCH (r:Room)
-  WHERE id(r) = 1
-  RETURN
-    r.name AS Name,
-    id(r) AS Id`)
+query = `MATCH (d:Dungeon) RETURN d.name`
+
+result = await client.graph.query('dungeon', query)
+
+console.log(JSON.stringify(result))
+
+query = `
+  MATCH (max:Treasure)
+    WITH max(max.gp) AS maxgp
+  MATCH (r:Room)-[:CONTAINS]->(t:Treasure) WHERE t.gp = maxgp
+    WITH id(r) as dest_id
+  MATCH (start:Room), (stop:Room)
+    WHERE id(start) = 1 AND id(stop) = dest_id
+  RETURN shortestPath((start)-[:LEADS_TO*]->(stop))`
+
+result = await client.graph.query('dungeon', query)
 
 console.log(JSON.stringify(result))
 
