f6e83fd950bbf77909b385647af6500c9acfeb48
Guy Royse <guy@guyroyse.com>
2022-11-08 09:45:44 -0500

    better and better demo

--- code/demo.js
+++ code/demo.js
@@ -6,23 +6,34 @@ await client.connect()
 
 let result, query
 
-query = `MATCH (d:Dungeon) RETURN d.name`
+query = `MATCH (n) RETURN n`
+
+// query = `MATCH (d:Dungeon) RETURN d.name`
+
+// query = `MATCH (r:Room)-[:CONTAINS]->(n) RETURN id(r), r.name, id(n), labels(n), n.name`
+
+// query = `
+//   MATCH (max:Treasure)
+//     WITH max(max.gp) AS maxgp
+//   MATCH (r:Room)-[:CONTAINS]->(t:Treasure)
+//     WHERE t.gp = maxgp
+//     WITH id(r) as dest_id
+//   MATCH p = (start:Room)-[:LEADS_TO*]->(stop:Room)
+//     WHERE id(start) = 1 AND id(stop) = dest_id
+//   RETURN p, length(p) as len
+//   ORDER BY len ASC
+//   LIMIT 1`
+
+// query = `
+//   MATCH (max:Treasure)
+//     WITH max(max.gp) AS maxgp
+//   MATCH (r:Room)-[:CONTAINS]->(t:Treasure) WHERE t.gp = maxgp
+//     WITH id(r) as dest_id
+//   MATCH (start:Room), (stop:Room)
+//     WHERE id(start) = 1 AND id(stop) = dest_id
+//   RETURN shortestPath((start)-[:LEADS_TO*]->(stop))`
 
 result = await client.graph.query('dungeon', query)
-
-console.log(JSON.stringify(result))
-
-query = `
-  MATCH (max:Treasure)
-    WITH max(max.gp) AS maxgp
-  MATCH (r:Room)-[:CONTAINS]->(t:Treasure) WHERE t.gp = maxgp
-    WITH id(r) as dest_id
-  MATCH (start:Room), (stop:Room)
-    WHERE id(start) = 1 AND id(stop) = dest_id
-  RETURN shortestPath((start)-[:LEADS_TO*]->(stop))`
-
-result = await client.graph.query('dungeon', query)
-
 console.log(JSON.stringify(result))
 
 await client.quit()
